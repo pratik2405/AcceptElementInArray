87457115565c85b58de8bc38f7e1ed1abb96a1c6
pratik2405 <shinde7388pratik@gmail.com>
2024-11-07 11:55:03 +0530

    Add project files.

---
+++ AcceptElementInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptElementInArray
+{
+    internal class Program
+    {
+        //Accept the 5 elements from user & display
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[5];
+            Console.WriteLine("Enter % element in array");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("--------Element in arr----------");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
